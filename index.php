d4fbfb3332863075ec135b58f846b3a5eec43451
Hiroshi Isobe <isobe69b@gmail.com>
2021-12-28 12:16:06 -0800

    front page main image and title styled

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php get_header(); ?>
 
-<h1>SAYO ONISHI OFFICIAL WEBSITE</h1>
+<div class="title-wrapper">
+    <h1><span class="main-title">SAYO ONISHI</span><br><span class="sub-title">OFFICIAL WEBSITE</span></h1>
+</div>
 
 <div class="main-pic-wrapper">
     <img class="main-image" src="<?php bloginfo('stylesheet_directory'); ?>/images/top.jpg" alt="main image">
