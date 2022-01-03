735ed9a505b5f353eaeaad0f24f8f8391b6a2dd3
Hiroshi Isobe <isobe69b@gmail.com>
2022-01-02 21:05:49 -0800

    div with 'main-pic-menu-wrapper' class added with display flex. Styling for mobile and tablet version completed.

--- index.php
+++ index.php
@@ -3,19 +3,17 @@
 <div class="title-wrapper">
     <h1><span class="main-title">SAYO ONISHI</span><br><span class="sub-title">OFFICIAL WEBSITE</span></h1>
 </div>
-
-<div class="main-pic-wrapper">
-    <img class="main-image" src="<?php bloginfo('stylesheet_directory'); ?>/images/top.jpg" alt="main image">
+<div class="main-pic-menu-wrapper">
+    <div class="main-pic-wrapper">
+        <img class="main-image" src="<?php bloginfo('stylesheet_directory'); ?>/images/top.jpg" alt="main image">
+    </div>
+    <?php
+        wp_nav_menu( array(
+            'theme_location'    =>  'menu-main',
+            'container'         =>  'nav',
+            'container_class'   =>  'menu-container',
+            'menu_class'        =>  'main-menu'
+        ) );
+        ?>
 </div>
-
-<?php
-      wp_nav_menu( array(
-          'theme_location'    =>  'menu-main',
-          'container'         =>  'nav',
-          'container_class'   =>  'menu-container',
-          'menu_class'        =>  'main-menu'
-      ) );
-    ?>
-
-
 <?php get_footer(); ?>
